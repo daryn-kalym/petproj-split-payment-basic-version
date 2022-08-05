eacc7083896cc5badafacb699f4ea5f6ffa90f4a
Daryn Kalym <daryn.kalym@Daryns-MacBook-Pro.local>
2022-08-05 19:02:38 +0600

    Initial commit of the project created long ago...

---
+++ main.py
@@ -0,0 +1,5 @@
+import pandas as numpy
+persons = ["Daryn","Miras","Yerzhan"]
+items = ["item1","item2","item3","item4"]
+costs = [3000,2000,3500,1500]
+links = {"Daryn":["item1","item3","item4"],"Miras":}
